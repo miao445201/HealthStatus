8f9c9ab5db5325fdb315aa0693079d2e0bd973cc
miao445201 <870291020@qq.com>
2016-03-31 17:30:51 +0800

    喜欢按钮（动画暂时放一放）

--- HealthStatus/Constant.h
+++ HealthStatus/Constant.h
@@ -31,6 +31,8 @@
 #define SymEmptyImage       [UIImage imageNamed:@"Active State-50.png"]
 #define ForwardImage        [UIImage imageNamed:@"body.png"]
 #define BackImage           [UIImage imageNamed:@"back.png"]
+#define LikeImage           [UIImage imageNamed:@"Like Filled-50.png"]
+#define NotLikeImage        [UIImage imageNamed:@"Like-50 .png"]
 #define RGBA(r,g,b,a) [UIColor colorWithRed:r/255.0f green:g/255.0f blue:b/255.0f alpha:a]
 
 #define kCommonParamsDict   [NSMutableDictionary dictionaryWithObjectsAndKeys:nil];
